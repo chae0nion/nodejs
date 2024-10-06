e8d0b6a0efeaffebbf28a174aaa7f38175d27e7c
chaewon <lisatell92@gmail.com>
2024-10-07 04:10:50 +0900

    프런트 단 javascript 만들기기

--- app/app.js
+++ app/app.js
@@ -5,8 +5,9 @@ const app = express();
 //라우팅
 const home = require("./src/routes/home");
 //앱세팅
-app.set("views", "./src/views")
-app.set("view engine", "ejs")
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use('/',home);// use는 미들웨어 등록해주는 메소드
 //루트 경로로 들어오면 routes\home으로 이동
