754b135c17b068d8a7ca811ca315f544d9f7e3cc
chaewon <lisatell92@gmail.com>
2024-10-07 17:23:54 +0900

    로그인 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,10 @@ function login(){
         psword: psword.value,
 
     };
+    console.log(req);
+    console.log(JSON.stringify(req));
+    // fetch("/login",{
+    //     body: JSON.stringify(req)
+    // })
     
 }
